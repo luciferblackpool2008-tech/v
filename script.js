615c786244c87620ca47104e1b453c1342b03107
luciferblackpool2008-tech <lucifer.blackpool2008@gmail.com>
2026-02-14 14:08:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function checkPassword() {
+  const pass = document.getElementById("password").value;
+  if (pass === "love") {   // change password here
+    document.getElementById("lockScreen").style.display = "none";
+    document.getElementById("mainContent").style.display = "block";
+    document.getElementById("bgMusic").play();
+  } else {
+    alert("Wrong password 💔");
+  }
+}
 
